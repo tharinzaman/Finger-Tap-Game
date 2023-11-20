48d7ba46aa712989070383efd66ffcfdb67f5197
tharinzaman <107755507+tharinzaman@users.noreply.github.com>
2023-11-20 17:29:31 +0000

    Initial Commit

---
+++ Finger Tap Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finger Tap Game
+//
+//  Created by Tharin Zaman on 20/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
