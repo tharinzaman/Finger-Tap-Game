082f52efe34de2a8cca036696360a833396a3ed8
tharinzaman <107755507+tharinzaman@users.noreply.github.com>
2023-12-04 19:19:16 +0000

    Alert added

---
+++ Finger Tap Game/model/utils/AlertKeysEnum.swift
@@ -0,0 +1,8 @@
+//
+//  AlertKeysEnum.swift
+//  Finger Tap Game
+//
+//  Created by Tharin Zaman on 04/12/2023.
+//
+
+import Foundation
---
+++ Finger Tap Game/model/utils/Strings.swift
@@ -0,0 +1,8 @@
+//
+//  Strings.swift
+//  Finger Tap Game
+//
+//  Created by Tharin Zaman on 04/12/2023.
+//
+
+import Foundation
